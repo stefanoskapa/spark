caf7f332b905e26520a073c8e861dafcf96e8817
stefanoskapa <koutsouflakis.stefanos@gmail.com>
2024-03-15 18:50:57 +0200

    update header file

--- spark.h
+++ spark.h
@@ -2,6 +2,36 @@
 #define BOARD_H
 
 #define U64 unsigned long long
+#define enc_piece 		0xF
+#define enc_source		0x3F0
+#define enc_target    0xFC00
+#define enc_prom      0xF0000
+#define enc_capture   0x100000
+#define enc_double    0x200000
+#define enc_ep        0x400000
+#define enc_cast      0x800000
+
+
+#define encode_move(piece, source, target, prom_piece, capture, double_push, ep, castling) \
+  (piece) 					  |\
+	(source << 4) 		  |\
+	(target << 10) 		  |\
+	(prom_piece << 16)  |\
+	(capture << 20)     |\
+  (double_push << 21) |\
+  (ep << 22) 					|\
+  (castling << 23)
+
+
+#define get_move_piece(move)         ((move & enc_piece )    >> 0)
+#define get_move_source(move)        ((move & enc_source)    >> 4)
+#define get_move_target(move)        ((move & enc_target)    >> 10)
+#define get_move_promotion(move)     ((move & enc_prom)      >> 16)
+#define get_move_capture(move)       ((move & enc_capture))
+#define get_move_double(move)        ((move & enc_double))
+#define get_move_ep(move)            ((move & enc_ep))
+#define get_move_castling(move)      ((move & enc_cast))
+
 
 enum {
   a8,b8,c8,d8,e8,f8,g8,h8,
@@ -39,6 +69,10 @@ typedef struct int_stack {
   int items[400];
   int index;
 } int_stack;
+typedef struct {
+  int moves[256];
+  int current_index;
+} moves;
 
 extern char ascii_pieces[12];
 extern int char_pieces[];
@@ -50,15 +84,16 @@ extern int pos_side;
 extern int pos_ep;
 extern int pos_castling;
 extern int first_pos_ep;
+
+
+void init_attack_tables();
+int generate_moves(moves *glist); 
 int isKingInCheck(int side);
 void make_move(int move);
 void takeback();
 void show_board();
+void print_bitboard(U64 bitboard);
 void parse_fen(char *fen_string); 
 void push(int_stack *is, int item);
 int pop(int_stack *is);
-typedef struct {
-  int moves[256];
-  int current_index;
-} moves;
 #endif
