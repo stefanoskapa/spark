032c0f3f0c08c0457a5df6a20fc7286ec79dc5cd
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2024-03-29 16:19:12 +0200

    github workflow

--- src/main.c
+++ src/main.c
@@ -22,37 +22,37 @@ struct perf_test {
 };
 
 struct perf_test pos_list[] = { // results taken from chessprogramming.org/Perft_Results
-    {
-      "Position 1", 
-      "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
-      {20ULL, 400ULL, 8902ULL, 197281ULL, 4865609ULL, 119060324ULL, 3195901860ULL, 84998978956ULL}, 8
-    },
-    {
-      "Position 2",
-      "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -",
-      {48ULL, 2039ULL, 97862ULL, 4085603ULL, 193690690ULL, 8031647685ULL}, 6
-    },
-    {
-      "Position 3",
-      "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -",
-      {14ULL, 191ULL, 2812ULL, 43238ULL, 674624ULL, 11030083ULL, 178633661ULL, 3009794393ULL}, 8
-    },
-    {
-      "Position 4",
-      "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
-      {6ULL, 264ULL, 9467ULL, 422333ULL, 15833292ULL, 706045033ULL}, 6
-    },
-    {
-      "Position 5",
-      "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
-      {44ULL, 1486ULL, 62379ULL, 2103487ULL, 89941194ULL}, 5
-    },
-    {
-      "Position 6",
-      "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
-      {46ULL, 2079ULL, 89890ULL, 3894594ULL, 164075551ULL, 6923051137ULL, 287188994746ULL}, 7
-    }
-  };
+  {
+    "Position 1", 
+    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+    {20ULL, 400ULL, 8902ULL, 197281ULL, 4865609ULL, 119060324ULL, 3195901860ULL, 84998978956ULL}, 8
+  },
+  {
+    "Position 2",
+    "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -",
+    {48ULL, 2039ULL, 97862ULL, 4085603ULL, 193690690ULL, 8031647685ULL}, 6
+  },
+  {
+    "Position 3",
+    "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - -",
+    {14ULL, 191ULL, 2812ULL, 43238ULL, 674624ULL, 11030083ULL, 178633661ULL, 3009794393ULL}, 8
+  },
+  {
+    "Position 4",
+    "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1",
+    {6ULL, 264ULL, 9467ULL, 422333ULL, 15833292ULL, 706045033ULL}, 6
+  },
+  {
+    "Position 5",
+    "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8",
+    {44ULL, 1486ULL, 62379ULL, 2103487ULL, 89941194ULL}, 5
+  },
+  {
+    "Position 6",
+    "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10",
+    {46ULL, 2079ULL, 89890ULL, 3894594ULL, 164075551ULL, 6923051137ULL, 287188994746ULL}, 7
+  }
+};
 
 
 
@@ -61,35 +61,41 @@ int main() {
   printf("engine started\n\n");
   init_attack_tables();
   benchmark();  
-  perft_suite(4);
+  perft_suite(5);
   return 0;
 }
 
 
-  
+
 void benchmark() {
+  printf("\n--> Benchmarking (Initial Position)...\n");
   parse_fen(pos_list[0].pos);
   run_perft(6);
 }
 
 void perft_suite(int max_depth) {
   
- 
+  printf("\n--> Running perft positions\n");
+
   int pos_count = sizeof(pos_list) / sizeof(pos_list[0]);
   for (int i = 0; i < pos_count; i++) {
-    printf("[ %s ]\n",pos_list[i].title);
+    printf("\n[ %s ]\n",pos_list[i].title);
     parse_fen(pos_list[i].pos);
     for (int j = 0; j <= max_depth && j < pos_list[i].d_count; j++) {
+      printf("depth %d: ", j + 1);
+      fflush(stdout);
       if (perft(j + 1) != pos_list[i].nodes[j]) {
         printf("failed :(\n");
-	return;
+        exit(1);
+      } else {
+        printf("success :)\n");
       }
     }
-    printf("success :)\n\n");
-
   }
+
+  printf("\nAll tests passed!\n");
 }
- 
+
 void run_perft(int depth) {
 
   U64 nodes;
