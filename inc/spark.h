a0d979fcf14d6c9046d5d51879b3bddaa3039144
Stefanos Koutsouflakis <koutsouflakis.stefanos@gmail.com>
2024-07-28 15:11:56 +0300

    Refactor

--- inc/spark.h
+++ inc/spark.h
@@ -13,6 +13,14 @@
  */
 #define MOVE uint32_t
 
+/**
+ * @brief Struct for storing the generator results
+ */
+typedef struct MoveList {
+ MOVE moves[256];
+ int current_index;
+ int capture_count;
+} MoveList;
 
 /** @brief Initializes attack tables for all pieces
  *
@@ -21,6 +29,81 @@
  *
  */
 void init_attack_tables(void);
+
+
+void parse_fen(char *fen_string);
+
+MoveList generate_moves(void);
+
+char *get_move_UCI(MOVE move);
+
+void make_move(int move);
+void takeback(void);
+
+#define ENCODE_MOVE(piece, source, target, prom_piece, capture, double_push, ep, castling) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10) 		  |\
+(prom_piece << 16)  |\
+(capture << 20)     |\
+(double_push << 21) |\
+(ep << 22) 					|\
+(castling << 23)
+
+#define ENCODE_EP(piece, source, target) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10) 		  |\
+(1ULL << 20)        |\
+(1ULL << 22)
+
+#define ENCODE_PROM(piece, source, target, prom_piece) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10) 		  |\
+(prom_piece << 16)
+
+#define ENCODE_DOUBLE(piece, source, target) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10) 		  |\
+(1ULL << 21)
+
+#define ENCODE_SIMPLE_MOVE(piece, source, target) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10)
+
+#define ENCODE_SIMPLE_CAPTURE(piece, source, target) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10) 		  |\
+(1ULL << 20)
+
+#define ENCODE_CASTLING(piece, source, target) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10) 		  |\
+(1ULL << 23)
+
+#define ENCODE_CAP_PROM(piece, source, target, prom_piece) \
+(piece) 					  |\
+(source << 4) 		  |\
+(target << 10) 		  |\
+(prom_piece << 16)  |\
+(1ULL << 20)
+
+#define GET_MOVE_PIECE(move)         ((move & 0xF )    >> 0)
+#define GET_MOVE_SOURCE(move)        ((move & 0x3F0)    >> 4)
+#define GET_MOVE_TARGET(move)        ((move & 0xFC00)    >> 10)
+#define GET_MOVE_PROMOTION(move)     ((move & 0xF0000)      >> 16)
+#define GET_MOVE_CAPTURE(move)       ((move & 0x100000))
+#define GET_MOVE_DOUBLE(move)        ((move & 0x200000))
+#define GET_MOVE_EP(move)            ((move & 0x400000))
+#define GET_MOVE_CASTLING(move)      ((move & 0x800000))
+#define GET_MOVE_CHECK(move)         ((move & 0x1000000))
+
+#define SET_MOVE_CHECK(move)         ((move | 0x1000000))
 
 
 
--- src/move_encoding/move_encoding.h
+++ src/move_encoding/move_encoding.h
@@ -18,17 +18,6 @@
 */
 
 
-
-#define ENC_PIECE 		0xF
-#define ENC_SOURCE		0x3F0
-#define ENC_TARGET    0xFC00
-#define ENC_PROM      0xF0000
-#define ENC_CAPTURE   0x100000
-#define ENC_DOUBLE    0x200000
-#define ENC_EP        0x400000
-#define ENC_CAST      0x800000
-#define ENC_CHECK     0x1000000
-
 #define ENCODE_MOVE(piece, source, target, prom_piece, capture, double_push, ep, castling) \
 	(piece) 					  |\
 	(source << 4) 		  |\
@@ -82,17 +71,17 @@
 	(prom_piece << 16)  |\
 	(1ULL << 20)
 
-#define GET_MOVE_PIECE(move)         ((move & ENC_PIECE )    >> 0)
-#define GET_MOVE_SOURCE(move)        ((move & ENC_SOURCE)    >> 4)
-#define GET_MOVE_TARGET(move)        ((move & ENC_TARGET)    >> 10)
-#define GET_MOVE_PROMOTION(move)     ((move & ENC_PROM)      >> 16)
-#define GET_MOVE_CAPTURE(move)       ((move & ENC_CAPTURE))
-#define GET_MOVE_DOUBLE(move)        ((move & ENC_DOUBLE))
-#define GET_MOVE_EP(move)            ((move & ENC_EP))
-#define GET_MOVE_CASTLING(move)      ((move & ENC_CAST))
-#define GET_MOVE_CHECK(move)         ((move & ENC_CHECK))
+#define GET_MOVE_PIECE(move)         ((move & 0xF )    >> 0)
+#define GET_MOVE_SOURCE(move)        ((move & 0x3F0)    >> 4)
+#define GET_MOVE_TARGET(move)        ((move & 0xFC00)    >> 10)
+#define GET_MOVE_PROMOTION(move)     ((move & 0xF0000)      >> 16)
+#define GET_MOVE_CAPTURE(move)       ((move & 0x100000))
+#define GET_MOVE_DOUBLE(move)        ((move & 0x200000))
+#define GET_MOVE_EP(move)            ((move & 0x400000))
+#define GET_MOVE_CASTLING(move)      ((move & 0x800000))
+#define GET_MOVE_CHECK(move)         ((move & 0x1000000))
 
-#define SET_MOVE_CHECK(move)         ((move | ENC_CHECK))
+#define SET_MOVE_CHECK(move)         ((move | 0x1000000))
 
 char *get_move_UCI(MOVE move);
 
